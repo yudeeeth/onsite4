e853e7da4f939eb19cafdf38d9fa8b8b1e699706
yudeeeth <udithkumarvd@gmail.com>
2020-10-05 20:35:23 +0530

    decription but its not working

--- blow.py
+++ blow.py
@@ -3,7 +3,7 @@ import math as Math
 import sys
 
 class blowcript():
-    def __init__(self,key,message,ende):
+    def __init__(self,key,message):
         self.thritytwoones = ((1<<32)-1)
         self.eightones = ((1<<8)-1)
         self.p=[]
@@ -15,7 +15,6 @@ class blowcript():
         key=int(key)
         self.key=int(key)
         message=int(message)
-        #message=intoascii(message)
         for i in range(256):
             self.s0.append(Math.floor(random.random()*2**31))
             self.s1.append(Math.floor(random.random()*2**31))
@@ -30,19 +29,22 @@ class blowcript():
                 buff = key & self.thritytwoones
                 self.p.append(buff)
             key=key>>32
-            #print(bin(buff))
-        if ende == "-d":
-            self.reverse()
+        right = message & self.thritytwoones
+        left = (message>>32) & self.thritytwoones
+        print(self.round(left,right))
+        self.reverse()
+        message = self.round(left,right)
         right = message & self.thritytwoones
         left = (message>>32) & self.thritytwoones
         print(self.round(left,right))
 
-    # def reverse():
-    #     for i in range(9):
-    #         temp = self.p[i]
-    #         self.p[]
-    #         pass
+    def reverse(self):
+        for i in range(9):
+            temp = self.p[i]
+            self.p[0]= self.p[-(i+1)]
+            self.p[-(i+1)]=temp
 
+    
     def sbox(self,n,val):
         if n==3:
             return self.s3[val]
@@ -55,8 +57,8 @@ class blowcript():
 
     def round(self,left,right,n=0):
         if n==16:
-            rig = left ^ self.p[16]
-            lef = right ^ self.p[17]
+            rig = right ^ self.p[16]
+            lef = left ^ self.p[17]
             return (lef<<32) + rig
         else:
             temp=self.fun(left,self.p[n])
@@ -80,7 +82,7 @@ class blowcript():
 
         
 print(sys.argv[1],sys.argv[2])
-obj = blowcript(sys.argv[1],sys.argv[2],sys.argv[3])
+obj = blowcript(sys.argv[1],sys.argv[2])
 
 
 
